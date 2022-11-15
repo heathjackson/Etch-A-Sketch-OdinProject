bccde8211b6bf83f4c2df86962e85800890e712f
Heather Jackson <heather.jackson2003@gmail.com>
2022-11-14 21:54:54 -0500

    black gradient and color working, now figuring out how to reset when each choice is clicked

--- etchASketch.js
+++ etchASketch.js
@@ -27,28 +27,30 @@ function hoverRandomColor() {
       color1 = Math.random() * 255;
       color2 = Math.random() * 255;
       color3 = Math.random() * 255;
+      hoverEffect.style.opacity = '';
       hoverEffect.style.backgroundColor = `rgb(${color1}, ${color2}, ${color3})`;
     });
   });
 }
 
+
 function hoverGradientBlack() {
-  const hoverBlackEffects = document.querySelectorAll('.hoverBlackEffect');
+  const hoverBlackEffects = document.querySelectorAll('.hoverBlackEffect')
+ 
 
   hoverBlackEffects.forEach(hoverBlackEffect => {
-    let opac = 0;
+    let opac = 0.9;
     hoverBlackEffect.addEventListener('mouseover', (event) => {
-      console.log(event.target.style);
+
   
-      if (event.target.style.backgroundColor === "" || event.target.style.opacity === "") {
-        console.log(event.target.style.backgroundColor = 'rgb(0, 0, 0)');
-        console.log(event.target.style.opacity = '0');
+      if (event.target.style.opacity === "") {
+        event.target.style.backgroundColor = 'rgb(255, 255, 255)';
+        event.target.style.opacity = '0.9';
       }
       else {
-        console.log(opac += 0.2);
-        console.log(event.target.style.opacity = opac);
+        opac -= 0.1;
+        event.target.style.opacity = opac;
       }
-      //event.target.style.border = 'rgba(0, 0, 0, 1)';
     });
   });
 }
@@ -66,6 +68,7 @@ function createDivs(gridSize) {
 function createColumns(length) {
   const allSquares = document.querySelector('#grid');
   allSquares.style.gridTemplateColumns = `repeat(${length}, 1fr)`;
+  allSquares.style.backgroundColor = "black";
 }
 
 function removeItems() {
